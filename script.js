5a07ee3a808b1f4f324f824bd917dae5fbd6e8e9
Alabhya Pandey <alabhyap@gmail.com>
2018-06-11 13:05:57 +0530

    final commit before submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-var portfolio = document.getElementById('portfolio');
-portfolio.onclick = function(){
-    window.open('https://alabhya139.github.io','_blank');
+
+/* manage click event on project section portfolio*/
+let portfolio = document.getElementById('portfolio');
+portfolio.onclick = () => {
+    window.open('https://alabhya139.github.io', '_blank');
 }
 
-var prajjwalan = document.getElementById('prajjwalan');
-prajjwalan.onclick = function(){
-    window.open('https://play.google.com/store/apps/details?id=com.prajjawalan.prajjwalan','_blank')
+/*manage click event on project section prajjwalan*/
+let prajjwalan = document.getElementById('prajjwalan');
+prajjwalan.onclick = () => {
+    window.open('https://play.google.com/store/apps/details?id=com.prajjawalan.prajjwalan', '_blank')
 }
