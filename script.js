54beb285b3bf3d555aa5d056a7350c4e0f72fccd
Alabhya Pandey <alabhyap@gmail.com>
2018-06-18 01:03:48 +0530

    added some dynamic features and animation

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ let prajjwalan = document.getElementById('prajjwalan');
 prajjwalan.onclick = () => {
     window.open('https://play.google.com/store/apps/details?id=com.prajjawalan.prajjwalan', '_blank')
 }
+
+/*manage click event on project section prajjwalan*/
+let wapp = document.getElementById('whatsapp-clone');
+wapp.onclick = () => {
+    window.open('https://alabhya139.github.io/whatsapp_clone', '_blank')
+}
+
+$(document).ready(()=>{
+    $('#toggle-button').click(()=>{
+        console.log("clicked");
+        $('.nav-bar').slideToggle('slow');
+        $(document).click(function(e) {
+            if (!$(e.target).is('#toggle-button')) {
+                $('.nav-bar').hide('slow');	    
+            }
+        });
+    });
+
+    if ($(window).width() <= 710) {
+        $('.nav-bar').hide();
+    }else $('.nav-bar').show();
+
+    $(window).on('resize',()=> {
+        var win = $(this);
+        if ($(win).width() <= 710) {
+            $('.nav-bar').hide();
+        }else $('.nav-bar').show();
+    });
+});
